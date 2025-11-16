6019324894d2f382f8da86a85770d65c420cdb5f
maicol-monge <maicol.monge@catolica.edu.sv>
2025-11-16 12:19:56 -0600

    Mejoras de Guia Home y Resenas

--- routes/waylo/homeRoutes.js
+++ routes/waylo/homeRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { homeGuia } = require('../../controllers/waylo/homeGuiaController');
-const authMiddleware = require('../../middleware/authMiddleware');
+const { requireAuth } = require('../../middleware/authMiddleware');
 
 // Agregamos autenticación para proteger datos del guía.
-router.get('/guia/:id_perfil_guia', authMiddleware, homeGuia);
+router.get('/guia/:id_perfil_guia', requireAuth, homeGuia);
 
 module.exports = router;
